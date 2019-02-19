55c6cb0bb784b4ff486cd3edebf73d909c077616
Jagiella, Stanislaw <stanislaw.jagiella.ctr@sabre.com>
2019-02-19 19:59:02 +0100

    fix unit test

--- test/components/main-app/main-app.test.tsx
+++ test/components/main-app/main-app.test.tsx
@@ -1,40 +1,15 @@
 import * as React from 'react'
 import * as TestRenderer from 'react-test-renderer';
 import {MainAppType} from 'type/components/main-app/main-app.type'
-import {createElement, MouseEventHandler, ReactDOM, useState} from 'react';
+import {MouseEventHandler, ReactElement, useState} from 'react';
+import MainApp from 'app/components/main-app/main-app';
+import {ReactTestRenderer} from 'react-test-renderer';
 
-describe('Test', () => {
-    it('Should test', () => {
+describe('<MainApp />', () => {
+    it('should render static content with div.main-app', () => {
+        const mainAppWrap: ReactTestRenderer = TestRenderer.create(<MainApp/>)
+        const mainDivWrapper = mainAppWrap.root.findByType('div')
 
-        const ff = (mem: number, setmem: React.Dispatch<number>) => (e: React.MouseEvent) => {
-            return setmem(mem+1)
-        }
-
-        const AA: MainAppType = () => {
-            let [mem, setmem] = useState<number>(10)
-            console.log('SJA: rendering', mem)
-            const f = ff(mem, setmem)
-
-            const t: MouseEventHandler<HTMLDivElement> = (e) => {
-                return
-            }
-
-
-            return (
-                <div onClick={f}>aa</div>
-            )
-        }
-        const rootComponent = TestRenderer.create(<AA/>).root
-        const div = rootComponent.findByType('div')
-        TestRenderer.act(() => {
-            div.props['onClick']()
-        })
-        TestRenderer.act(() => {
-            div.props['onClick']()
-        })
-        TestRenderer.act(() => {
-            div.props['onClick']()
-        })
-
+        expect(mainDivWrapper.props.className).toEqual('main-app')
     })
 })
