ad72675f85a6f3c5b5548df0e8f77eac8d754c01
Jagiella, Stanislaw <stanislaw.jagiella.ctr@sabre.com>
2019-03-04 09:10:52 +0100

    Update versions

--- app/containers/two-values-parser.tsx
+++ app/containers/two-values-parser.tsx
@@ -1,12 +1,8 @@
 import {ParseFnType} from 'type/containers/two-values/two-values-container.type';
 
 export const parse: ParseFnType = (value) => {
-    const numberValue: number = parseInt(value)
     if (/^\d+$/.test(value) == false) {
         return null
     }
-    if (isNaN(numberValue)) {
-        return null
-    }
-    return numberValue;
+    return parseInt(value);
 }
