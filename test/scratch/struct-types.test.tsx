646cfc42a4e98d89d9016766a88b19aabe805cd1
Jagiella, Stanislaw <stanislaw.jagiella.ctr@sabre.com>
2019-04-02 10:45:14 +0200

    + upgrading packages + adding scratch

---
+++ test/scratch/struct-types.test.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+describe('structural types tests', () => {
+    it('should call b method', () => {
+        type NarrowType = {
+            a: number,
+            b: () => void
+        }
+
+        type WideType = {
+            a: number,
+            b: () => void,
+            c: string
+        }
+
+        let narrowObj: NarrowType = {
+            a: 10,
+            b: () => {
+                console.log('Im narrowType')
+            }
+        }
+
+        let wideObj: WideType = {
+            a: 100,
+            b: () => {
+                console.log('Im wideType')
+            },
+            c: 'TypeScript'
+        }
+
+        narrowObj = wideObj
+
+        // narrowObj.b()
+        // console.log('a value is:', narrowObj.a)
+    })
+
+    it('should not overwrite method', () => {
+        // type NarrowType = {
+        //     a: number,
+        //     b: () => void
+        // }
+        //
+        // type WideType = {
+        //     a: number,
+        //     b: () => void,
+        //     c: string
+        // }
+        //
+        // let narrowObj: NarrowType = {
+        //     a: 10,
+        //     b: () => {
+        //         console.log('Im narrowType')
+        //     }
+        // }
+        //
+        // let wideObj: WideType = {
+        //     a: 10,
+        //     b: () => {
+        //         console.log('Im wideType')
+        //     },
+        //     c: 'TypeScript'
+        // }
+        //
+        // function fun(obj: NarrowType){
+        //     obj.b()
+        // }
+        //
+        // function fun(obj: WideType){
+        //     obj.b()
+        // }
+    })
+});
