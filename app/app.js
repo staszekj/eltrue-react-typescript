122c27800ca0370590530e00ac1015efb42e9987
Jagiella, Stanislaw <stanislaw.jagiella.ctr@sabre.com>
2019-04-24 12:38:11 +0200

    + add store to react context

--- app/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MainApp from "app/components/main-app";
-
-ReactDOM.render(
-    <MainApp/>,
-
-  document.querySelector('#main-app')
-);
---
+++ app/app.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import thunk from 'redux-thunk';
+import MainApp from "app/components/main-app";
+import {applyMiddleware, combineReducers, createStore, compose} from 'redux';
+import {twoBarsReducer} from './redux/two-bars';
+import {Provider} from 'react-redux';
+
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const reducers = combineReducers({
+    twoBars: twoBarsReducer
+});
+export const store = createStore(
+    reducers,
+    {},
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <MainApp/>
+    </Provider>,
+
+    document.querySelector('#main-app')
+)
